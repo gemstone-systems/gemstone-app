c431f74592a4a8a9b724d77229e2e679b3afe723
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-10-11 22:57:31 +0800

    refactor: reset project

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import ChatComponent from "@/app/components/ChatComponent";
+import { Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>Edit app/index.tsx to edit this screen.</Text>
+        </View>
+    );
+}
