bf64ffdfba60e3cb2e735c86b13760d0123ae365
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-10-14 19:18:23 +0800

    refactor: nicer messages

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -2,7 +2,7 @@ import type { DidPlc } from "@/lib/types/atproto";
 import { getBskyProfile } from "@/queries/get-profile";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
-import { Image, View, Text } from "react-native";
+import { Image, View, Text, StyleSheet } from "react-native";
 
 export const Avatar = React.memo(({ did }: { did: DidPlc }) => {
     const {
@@ -21,8 +21,21 @@ export const Avatar = React.memo(({ did }: { did: DidPlc }) => {
     ) : isError ? (
         <Text>:(</Text>
     ) : (
-        profile && <Image source={{ uri: profile.avatar ?? profile.handle }} />
+        profile && (
+            <Image
+                style={styles.avatar}
+                source={{ uri: profile.avatar ?? profile.handle }}
+            />
+        )
     );
 });
 
 Avatar.displayName = "Avatar";
+
+const styles = StyleSheet.create({
+    avatar: {
+        width: 32,
+        height: 32,
+        borderRadius: 2000000000,
+    },
+});
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -4,18 +4,25 @@ import { Text, View, StyleSheet } from "react-native";
 
 export const Message = ({ message }: { message: ShardMessage }) => {
     return (
-        <View style={styles.messageItem}>
-            <Text style={styles.messageText}>{message.text}</Text>
-            <Text style={styles.timestamp}>
-                {new Date(message.timestamp).toLocaleTimeString()}
-            </Text>
-            <Text style={styles.timestamp}>{message.did}</Text>
+        <View style={styles.messageItemContainer}>
             <Avatar did={message.did} />
+            <View style={styles.messageItem}>
+                <Text style={styles.messageText}>{message.text}</Text>
+                <Text style={styles.timestamp}>
+                    {new Date(message.timestamp).toLocaleTimeString()}
+                </Text>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    messageItemContainer: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "flex-end",
+    },
     messageItem: {
         marginBottom: 12,
         padding: 12,
