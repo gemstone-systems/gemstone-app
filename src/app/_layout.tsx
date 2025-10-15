17ca809073139a606517f1b665ab88993f76926a
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-10-16 01:39:33 +0800

    refactor!: redirect to ip loopback instead of localhost
    indexeddb fuckery apparently

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,28 @@
 import { Providers } from "@/providers";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { Platform } from "react-native";
 
 export default function RootLayout() {
+    useEffect(() => {
+        if (Platform.OS === "web" && typeof window !== "undefined") {
+            const hostname = window.location.hostname;
+            if (hostname === "127.0.0.1") return;
+            if (hostname === "localhost") {
+                const newUrl = window.location.href.replace(
+                    "localhost",
+                    "127.0.0.1",
+                );
+                window.location.replace(newUrl);
+                return;
+            }
+
+            throw new Error(
+                "don't use localhost, use 127.0.0.1. this shouldn't error unless you've done something very wrong.",
+            );
+        }
+    });
+
     return (
         <Providers>
             <Stack />
