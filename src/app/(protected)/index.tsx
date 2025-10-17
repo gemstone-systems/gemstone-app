23997d9b58c5449c4f171d44e6cf25fa8cb9450d
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-10-18 01:58:52 +0800

    refactor!: cleanup

--- src/app/(protected)/index.tsx
+++ src/app/(protected)/index.tsx
@@ -1,11 +1,9 @@
-import { Login } from "@/components/Auth/Login";
 import ChatComponentProfiled from "@/components/ChatComponentProfiled";
 import { useOAuthSession } from "@/providers/OAuthProvider";
-import { Redirect, useRouter } from "expo-router";
+import { Redirect } from "expo-router";
 import { View } from "react-native";
 
 export default function Index() {
-    const router = useRouter();
     const session = useOAuthSession();
     return (
         <View
