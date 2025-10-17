201881f8ce69582df94ab6fb18c415c45964ad4a
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-10-18 02:33:59 +0800

    feat: proper oauth scoping

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,7 +1,15 @@
+const __DEV__oAuthCallbackUrl = "http://127.0.0.1:8081/login/";
+
+const oAuthScopes = ["atproto", "transition:generic"];
+
+const oAuthScopesString = oAuthScopes
+    .reduce((prev, curr) => `${prev} ${curr}`)
+    .trim();
+
 export const __DEV__loopbackOAuthMetadata = {
-    client_id: `http://localhost?redirect_uri=${encodeURIComponent("http://127.0.0.1:8081/login/")}`,
-    redirect_uris: ["http://127.0.0.1:8081/login/"],
-    scope: "atproto",
+    client_id: `http://localhost?redirect_uri=${encodeURIComponent(__DEV__oAuthCallbackUrl)}&scope=${encodeURIComponent(oAuthScopesString)}`,
+    redirect_uris: [__DEV__oAuthCallbackUrl],
+    scope: oAuthScopesString,
     token_endpoint_auth_method: "none",
     response_types: ["code"],
     grant_types: ["authorization_code", "refresh_token"],
