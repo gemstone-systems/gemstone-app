9471a1fac1da4955bcb5bf501e76dc4795337f3e
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-11-04 13:26:18 +0800

    feat: better palette hook

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,3 +1,6 @@
+import { useFacet } from "@/lib/facet";
+import type { Facet } from "@/lib/facet/src/facet";
+import type { FacetPalette } from "@/lib/facet/src/palette";
 import type { Enumify } from "@/lib/utils/types";
 import type { Dispatch, ReactNode, SetStateAction } from "react";
 import { createContext, useContext, useState } from "react";
@@ -13,6 +16,7 @@ export type ColorMode = Enumify<typeof ColorMode>;
 interface ThemeContextValue {
     colorMode: ColorMode;
     setColorMode: Dispatch<SetStateAction<ColorMode>>;
+    currentPalette: FacetPalette;
 }
 
 const ThemeContext = createContext<ThemeContextValue | null>(null);
@@ -36,12 +40,29 @@ export const useSetColorMode = () => {
     return setColorMode;
 };
 
+export const useCurrentPalette = () => {
+    const { currentPalette } = useThemeProvider();
+    return currentPalette;
+};
+
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
     const [colorMode, setColorMode] = useState<ColorMode>(ColorMode.DARK);
+    const facet = useFacet();
+    const currentPalette =
+        colorMode === "dark"
+            ? facet.variants.obsidian
+            : // TODO: temporary. will remove the null coalesce when pearl is defined.
+              (facet.variants.pearl ?? facet.variants.obsidian);
+
+    // TODO: remove nullability with obsidian and pearl palettes.
+    // instead, these are provided as defaults and if we add more themes
+    // in the future, they can just be tacked on.
+    if (!currentPalette) throw new Error("Don't use light mode for now.");
 
     const value: ThemeContextValue = {
         colorMode,
         setColorMode,
+        currentPalette,
     };
     return <ThemeContext value={value}>{children}</ThemeContext>;
 };
