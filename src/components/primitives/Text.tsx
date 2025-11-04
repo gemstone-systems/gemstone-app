ae48109ee17c76ea4008078a5b56f5827ad306aa
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-11-04 16:26:07 +0800

    feat: default text colour

--- src/components/primitives/Text.tsx
+++ src/components/primitives/Text.tsx
@@ -1,4 +1,5 @@
 import { useFacet } from "@/lib/facet";
+import { useCurrentPalette } from "@/providers/ThemeProvider";
 import type { ReactNode } from "react";
 import type { StyleProp, TextStyle } from "react-native";
 // eslint-disable-next-line no-restricted-imports -- This is the component in question. We are re-exporting RN's base text component.
@@ -13,12 +14,17 @@ export const Text = ({
     style?: StyleProp<TextStyle>;
 }) => {
     const { typography } = useFacet();
+    const { semantic } = useCurrentPalette();
 
     return (
         <RnText
             {...props}
             style={[
-                { fontFamily: typography.families.primary, fontWeight: 300 },
+                {
+                    fontFamily: typography.families.primary,
+                    fontWeight: 300,
+                    color: semantic.text,
+                },
                 style,
             ]}
         >
