6c32c78b5bbf9f8add8ec9a8dac5254505cd97bd
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-11-04 14:58:32 +0800

    feat: our text component

---
+++ src/components/primitives/Text.tsx
@@ -0,0 +1,25 @@
+import { useFacet } from "@/lib/facet";
+import type { ReactNode } from "react";
+import type { StyleProp, TextStyle } from "react-native";
+// eslint-disable-next-line no-restricted-imports -- This is the component in question. We are re-exporting RN's base text component.
+import { Text as RnText } from "react-native";
+
+export const Text = ({
+    children,
+    style,
+    ...props
+}: {
+    children: ReactNode;
+    style?: StyleProp<TextStyle>;
+}) => {
+    const { typography } = useFacet();
+
+    return (
+        <RnText
+            {...props}
+            style={[{ fontFamily: typography.families.primary }, style]}
+        >
+            {children}
+        </RnText>
+    );
+};
