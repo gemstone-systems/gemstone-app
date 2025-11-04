7564f69d79ac0b17c793ff20608eb641ba14140c
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-11-04 16:25:32 +0800

    feat: expose screen options prop

--- src/components/primitives/Stack.tsx
+++ src/components/primitives/Stack.tsx
@@ -1,8 +1,18 @@
 import { useDebugState } from "@/providers/DebugProvider";
+import type { NativeStackNavigationOptions } from "@react-navigation/native-stack";
 import { Stack as ExpoStack } from "expo-router";
 
-export const Stack = () => {
+export const Stack = ({
+    screenOptions,
+    ...props
+}: {
+    screenOptions?: NativeStackNavigationOptions;
+}) => {
     const debugValue = useDebugState();
     const { showStackHeader } = debugValue;
-    return <ExpoStack screenOptions={{ headerShown: showStackHeader }} />;
+    return (
+        <ExpoStack
+            screenOptions={{ headerShown: showStackHeader, ...screenOptions }}
+        />
+    );
 };
