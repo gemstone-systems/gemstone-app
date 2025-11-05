290b8e880072ba435611b0f1cb4fada3d0505a4a
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-11-05 13:52:24 +0800

    refactor: stub welcome screen

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,5 +1,10 @@
+import { Text } from "@/components/primitives/Text";
 import { View } from "react-native";
 
 export const Home = () => {
-    return <View></View>;
+    return (
+        <View>
+            <Text>Welcome! Channels are on the left :D</Text>
+        </View>
+    );
 };
