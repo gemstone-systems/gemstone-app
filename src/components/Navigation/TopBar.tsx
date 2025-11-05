137574c7261476c2a683eb2787b593d0688a27a0
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-11-05 13:45:26 +0800

    refactor: top bar logo redirect to home

--- src/components/Navigation/TopBar.tsx
+++ src/components/Navigation/TopBar.tsx
@@ -1,10 +1,13 @@
 import { GmstnLogoColor } from "@/components/icons/gmstn/GmstnLogoColor";
+import { Link } from "expo-router";
 import { View } from "react-native";
 
 export const TopBar = () => {
     return (
-        <View style={{ padding: 8, paddingLeft: 12, paddingTop: 12 }}>
-            <GmstnLogoColor height={36} width={36} />
-        </View>
+        <Link href="/">
+            <View style={{ padding: 8, paddingLeft: 12, paddingTop: 12 }}>
+                <GmstnLogoColor height={36} width={36} />
+            </View>
+        </Link>
     );
 };
