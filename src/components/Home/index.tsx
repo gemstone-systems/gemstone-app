990dddad6598048616374818423c9e9d76b91c0c
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-11-06 12:36:56 +0800

    refactor: reword label

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,10 +1,36 @@
+import { Loading } from "@/components/primitives/Loading";
 import { Text } from "@/components/primitives/Text";
+import { useFacet } from "@/lib/facet";
+import { useProfile } from "@/providers/authed/ProfileProvider";
 import { View } from "react-native";
 
 export const Home = () => {
+    const { profile, isLoading } = useProfile();
+    const { typography } = useFacet();
+
     return (
-        <View>
-            <Text>Welcome! Channels are on the left :D</Text>
+        <View
+            style={{
+                flex: 1,
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            {isLoading ? (
+                <Loading />
+            ) : !profile ? (
+                <Text>Couldn&apos;t load profile :(</Text>
+            ) : (
+                <>
+                    <Text style={[typography.sizes.lg]}>
+                        Welcome to Gemstone!
+                    </Text>
+                    <Text style={[typography.sizes.lg]}>
+                        Channels are on the left :D
+                    </Text>
+                </>
+            )}
         </View>
     );
 };
