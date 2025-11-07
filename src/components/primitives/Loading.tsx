3984e2a357643674f1178b5f403b5bc13d82598a
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-11-07 18:29:35 +0800

    refactor: loading spinner size small

--- src/components/primitives/Loading.tsx
+++ src/components/primitives/Loading.tsx
@@ -2,11 +2,20 @@ import type { HexCode } from "@/lib/facet/src/lib/colors";
 import { useCurrentPalette } from "@/providers/ThemeProvider";
 import { ActivityIndicator, View } from "react-native";
 
-export const Loading = ({ color }: { color?: HexCode }) => {
+export const Loading = ({
+    color,
+    size,
+}: {
+    color?: HexCode;
+    size?: number | "large" | "small";
+}) => {
     const { semantic } = useCurrentPalette();
     return (
         <View>
-            <ActivityIndicator size="large" color={color ?? semantic.primary} />
+            <ActivityIndicator
+                size={size ?? "large"}
+                color={color ?? semantic.primary}
+            />
         </View>
     );
 };
